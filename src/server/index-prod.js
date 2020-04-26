0c6db83d7d32ba8cbdb2c8701b8b90fa0ca703ad
Hadar Rottenberg <hadar@gooddollar.org>
2020-04-26 09:27:16 +0300

    add: print concurrency

--- src/server/index-prod.js
+++ src/server/index-prod.js
@@ -1,4 +1,5 @@
 import throng from 'throng'
 import start from './server-prod'
 
+console.log('workers concurrency:', process.env.WEB_CONCURRENCY)
 throng({ workers: process.env.WEB_CONCURRENCY, lifetime: Infinity }, start)
