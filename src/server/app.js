aeb76327e5c3d9e94cc5329af2b2c615ec872d35
Glitch (copper-spicy-coat) <none>
2020-07-02 07:52:01 +0000

    🚌🗻 Checkpoint ./.env.test:18244851/4 ./.gitignore:18244851/16 ./src/server/app.js:18244851/20

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import middlewares from './server-middlewares'
 const app = express()
-
+console.log("XXXXX")
 app.use(express.static('public'))
 middlewares(app, 'prod')
 
